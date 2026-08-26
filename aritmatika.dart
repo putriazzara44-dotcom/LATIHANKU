cd220a48b3c341ac1572ab5f5b2d1aedbc4c0324
Azzahra Sri W. Putri <putriazzara44@gmail.com>
2026-08-27 04:20:11 +0900

    Add files via upload

---
+++ aritmatika.dart
@@ -0,0 +1,15 @@
+void main() {
+  int angka1 = 20;
+  int angka2 = 5;
+
+  int hasilTambah = angka1 + angka2;
+  int hasilKurang = angka1 - angka2;
+  int hasilKali = angka1 * angka2;
+  double hasilBagi = angka1 / angka2;
+
+  print('=== OPERASI ARITMATIKA ===');
+  print('Penjumlahan ($angka1 + $angka2) = $hasilTambah');
+  print('Pengurangan ($angka1 - $angka2) = $hasilKurang');
+  print('Perkalian ($angka1 * $angka2)   = $hasilKali');
+  print('Pembagian ($angka1 / $angka2)   = $hasilBagi');
+}
